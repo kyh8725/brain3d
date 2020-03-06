ca283f3026798de6ea5dbb1ec3eecf5e9697cd94
Yoonho Kim <Daniel@Yoonhos-MBP.lan>
2020-03-06 10:44:34 -0800

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+//import Helper from "./helper/helper";
+import data from "./models/data.json";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {};
+  getData = () => {
+    console.log(data);
+    const productArray = data.map(item => {
+      return item.country;
+    });
+    return productArray;
+  };
+  render() {
+    return <div className="App">{this.getData()}</div>;
+  }
 }
 
 export default App;
